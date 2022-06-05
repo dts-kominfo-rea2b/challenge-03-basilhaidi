6fe093210ae259065eb4c3e14c8b8ab70dbf7fb5
Basil Haidi <basilhaidi65@gmail.com>
2022-06-05 21:46:01 +0700

    modifikasi fungsi listBelanjaan dan totalBelanjaan

--- index.js
+++ index.js
@@ -1,40 +1,50 @@
 // ! JANGAN DIMOFIDIKASI
 const dataBelanjaan = [
   {
-    id: "D-10001",
-    nama: "Minyak Goreng Delima",
+    id: 'D-10001',
+    nama: 'Minyak Goreng Delima',
     harga: 25000,
     kuantitas: 2,
   },
   {
-    id: "D-10020",
-    nama: "Beras Mamos",
+    id: 'D-10020',
+    nama: 'Beras Mamos',
     harga: 48000,
     kuantitas: 1,
   },
   {
-    id: "F-00010",
-    nama: "Larutan Cap Kaki Empat",
+    id: 'F-00010',
+    nama: 'Larutan Cap Kaki Empat',
     harga: 7500,
     kuantitas: 8,
   },
 ];
 
 // boleh dimodifikasi bila ingin menggunakan deklarasi fungsi yang normal
-const listBelanjaan = null;
+const listBelanjaan = (data) => {
+  let detailBarang = [];
+  for (let i = 0; i < data.length; i++) {
+    detailBarang[i] = `- ${data[i].nama} x ${data[i].kuantitas}`;
+  }
+  return detailBarang;
+};
 
 // boleh dimodifikasi bila ingin menggunakan deklarasi fungsi yang normal
-const totalBelanjaan = null;
+const totalBelanjaan = (data) => {
+  let totalHarga = null;
+  for (let i = 0; i < data.length; i++) {
+    totalHarga += data[i].harga * data[i].kuantitas;
+  }
+  return totalHarga;
+};
 
 // ! JANGAN DIMODIFIKASI
 const main = () => {
-  console.log("Selamat datang customer");
+  console.log('Selamat datang customer');
 
-  console.log("Belanjaan Anda adalah:");
-  console.log(listBelanjaan?.(dataBelanjaan)?.join("\n"));
-  console.log(
-    "\nTotal Belanjaan Anda adalah Rp. " + totalBelanjaan?.(dataBelanjaan)
-  );
+  console.log('Belanjaan Anda adalah:');
+  console.log(listBelanjaan?.(dataBelanjaan)?.join('\n'));
+  console.log('\nTotal Belanjaan Anda adalah Rp. ' + totalBelanjaan?.(dataBelanjaan));
 };
 
 main();
